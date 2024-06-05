08a0da953fe417d90b2f6d19665927f66dff1293
Utkarsh Raj <120942858+UtRaj@users.noreply.github.com>
2024-06-05 14:57:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 import nltk
 import os
 
-# Set NLTK data directory
-nltk_data_dir = os.path.join(os.path.expanduser("~"), "nltk_data")
-nltk.data.path.append(nltk_data_dir)
-
-# Download NLTK data
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
-
 
 
 import streamlit as st
