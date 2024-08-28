45901f0fc0ae59e8059e69ccb6ad81c0e54ec6c7
Utkarsh Raj <120942858+UtRaj@users.noreply.github.com>
2024-08-28 23:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ nltk.data.path.append(nltk_data_dir)
 
 # Check if NLTK data is available, if not, download
 if not os.path.exists(os.path.join(nltk_data_dir, 'tokenizers/punkt')):
-    nltk.download('punkt', download_dir=nltk_data_dir)
+    nltk.download('punkt_tab', download_dir=nltk_data_dir)
 if not os.path.exists(os.path.join(nltk_data_dir, 'taggers/averaged_perceptron_tagger')):
     nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
 
